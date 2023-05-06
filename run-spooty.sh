f8fc911c615bc56d36b2fd6040b375cf709e6a6e
Kyle Hiller <kyle.hiller@gmail.com>
2023-05-06 11:27:17 -0500

    update run spooty script

--- run-spooty.sh
+++ run-spooty.sh
@@ -1,2 +1,4 @@
+docker compose down
 aws ssm get-parameter --with-decryption --name spootifer-env --region us-west-2 | jq -r '.Parameter.Value' > .env
+docker compose pull
 docker compose up --force-recreate --build --detach
