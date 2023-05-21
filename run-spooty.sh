64d1ab03400b15fe1c346542bbf78b81b6ab8460
Kyle Hiller <kyle.hiller@gmail.com>
2023-05-21 13:09:01 -0500

    remove some unnecessaru params and create volume in build

--- run-spooty.sh
+++ run-spooty.sh
@@ -1,1 +1,1 @@
-docker rm -f spootifer && docker run --name spootifer --mount type=volume,volume-driver=local,src=spootifer-local,dst=/db --env-file .env -dp 8080:8081  spootifer:latest
+docker rm -f spootifer && docker run --name spootifer --mount type=volume,src=spootifer-local,dst=/db --env-file .env -dp 8080:8081  spootifer:latest
