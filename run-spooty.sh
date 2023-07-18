1c0ee9becd6077036cfc13f36fa56ab5f36ffe84
Kyle Hiller <kyle.hiller@gmail.com>
2023-07-18 10:59:53 -0400

    adds local dev setup for spotify worker node

--- run-spooty.sh
+++ run-spooty.sh
@@ -1,1 +1,1 @@
-docker rm -f spootifer && docker run --name spootifer --mount type=volume,src=spootifer-local,dst=/db --env-file .env -dp 8080:8081  spootifer:latest
+docker-compose up --build --detach
